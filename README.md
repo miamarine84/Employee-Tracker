5723b68d388b62c6c89f71f1f07977e592278646
marco p <miamarine84@yahoo.com>
2020-09-27 19:47:15 -0400

    modified certain final comments and created a better video for the presentation

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Unit 12 MySQL Homework: Employee Tracker
 
 ![Generator](./employeeTracker.gif)
-
+[Click here for a better view]('https://drive.google.com/file/d/1MOFB_IkY3MqmskNQjayI_h5bE4P3WWr2/view')
 
 Developers are often tasked with creating interfaces that make it easy for non-developers to view and interact with information stored in databases. Often these interfaces are known as Content Management Systems. In this homework assignment, your challenge is to architect and build a solution for managing a company's employees using node, inquirer, and MySQL.
 
--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var connection = mysql.createConnection({
   user: "root",
 
   password: "root",
-  database: "employee"
+  database: "EMPLOYEE_TRACKER"
 });
 
 let questions = [
@@ -27,19 +27,20 @@ let questions = [
       'View All Employees',
       'View All Departments',
       'View All Managers',
+      'View All Roles',
       'Add Employee',
       'Add Departments',
       'Add Roles',
-      'Remove Employee',
-      'Update Employee Role',
+      'Update Employee',
+      'Remove Employee Role',
       'Update Employee Manager',
     ],
   },
 ];
 let value;
-function programStart() {
+const programStart = () => {
   inquirer.prompt(questions).then((answers) => {
-    console.log(JSON.stringify(answers, null, '  '));
+
     value = answers.firstChoice
     // Checking each value and sending them in to their correct functionality
     if (value == 'View All Employees') {
@@ -48,6 +49,8 @@ function programStart() {
       viewByDepartment();
     } else if (value == 'View All Managers') {
       viewByManager();
+    } else if (value == 'View All Roles') {
+      viewRoles();
     } else if (value == 'Add Employee') {
       addEmployee();
     } else if (value == 'Add Departments') {
@@ -56,8 +59,8 @@ function programStart() {
       addRoles();
     } else if (value == 'Remove Employee') {
       removeEmployee();
-    } else if ('Update Employee Role') {
-      updateRole();
+    } else if (value == 'Update Employee') {
+      updateEmployee();
     } else if (value == 'Update Employee Manager') {
       updateManager();
     }
@@ -65,7 +68,7 @@ function programStart() {
 };
 
 //Program Re-starter allowing the user to either start over or end the program
-function programRestarter() {
+const programRestarter = () => {
 
   let restart = [
     {
@@ -84,6 +87,7 @@ function programRestarter() {
     if (value == 'RE-START') {
       programStart()
     } else {
+      connection.end();
       console.log('Thanks hope to see you soon again!')
     }
   })
@@ -92,9 +96,9 @@ function programRestarter() {
 // Our functions when user makes his choice of what he wants to do in the program
 
 // User chooses to View Employees
-function viewAll() {
+const viewAll = () => {
   // if(err) throw err;
-  connection.query('SELECT * FROM employee.employee', function (error, results) {
+  connection.query('SELECT * FROM employee_tracker.employee', function (error, results) {
     if (error) {
       console.log(error)
     } else {
@@ -105,8 +109,8 @@ function viewAll() {
   programRestarter();
 }
 // View by Department
-function viewByDepartment() {
-  connection.query('SELECT EMPLOYEE.FIRST_NAME,EMPLOYEE.LAST_NAME,ROLE.ROLE_TITLE FROM EMPLOYEE INNER JOIN ROLE ON ROLE.ID=EMPLOYEE.ROLE_ID;', function (error, results) {
+const viewByDepartment = () => {
+  connection.query('SELECT * FROM DEPARTMENT;', function (error, results) {
     if (error) {
       console.log(error)
     } else {
@@ -117,9 +121,9 @@ function viewByDepartment() {
   programRestarter();
 }
 // View by Manager
-function viewByManager() {
-  console.log('this manager thing works!');
-  connection.query('SELECT * FROM employee.employee WHERE manager_id is not null;', function (error, results) {
+const viewByManager = () => {
+
+  connection.query('SELECT * FROM employee_tracker.employee WHERE manager_id is not null;', function (error, results) {
     if (error) {
       console.log(error)
     } else {
@@ -129,168 +133,185 @@ function viewByManager() {
   })
   programRestarter();
 }
+const viewRoles = () => {
+  connection.query('SELECT * FROM employee_tracker.role;', function (error, results) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log() // Creating a new line to show the results properly.
+      console.table(results);
+    }
+  })
+  programRestarter();
+}
 
 
 // Add employee
-function addEmployee() {
-  console.log('employee adder');
-  employeeAdder();
-  function employeeAdder() {
-    let employeeAddQuestions = [
-      {
-        type: 'input',
-        name: 'firstName',
-        message: "What's your first name?",
-      },
-      {
-        type: 'input',
-        name: 'lastName',
-        message: "What's your last name?",
-      },
-      {
-        type: "list",
-        message:
-          "Please choose your role correctly 1 --> Manager 2 --> Engineer 3 --> Janitor 4 --> Intern",
-        choices: [
-          1, 2, 3, 4,
-        ],
-        name: "role",
-      },
+
+const addEmployee = () => {
+  console.log('Welcome to Employee adder');
+  let employeeAddQuestions = [
+    {
+      type: 'input',
+      name: 'firstName',
+      message: "What's your first name?",
+    },
+    {
+      type: 'input',
+      name: 'lastName',
+      message: "What's your last name?",
+    },
+    {
+      type: "list",
+      message:
+        "Please choose your role correctly 1 --> Manager 2 --> Engineer 3 --> Janitor 4 --> Intern",
+      choices: [
+        1, 2, 3, 4,
+      ],
+      name: "role",
+    },
+  ]
+
+  inquirer.prompt(employeeAddQuestions).then((answers) => {
+    console.log(`${answers.firstName}, ${answers.lastName}, ${answers.role}`)
+    let managerArray = [
+      answers.firstName,
+      answers.lastName,
+      answers.role,
     ]
-
-    inquirer.prompt(employeeAddQuestions).then((answers) => {
-      console.log(`${answers.firstName}, ${answers.lastName}, ${answers.role}`)
-      let managerArray = [
-        answers.firstName,
-        answers.lastName,
-        answers.role,
+    if (answers.role == 1) {
+      let managerIdQuestion = [
+        {
+          type: 'input',
+          name: 'managerId',
+          message: "What's your manager ID?",
+        }
       ]
-      if (answers.role == 1) {
-        let managerIdQuestion = [
-          {
-            type: 'input',
-            name: 'managerId',
-            message: "What's your manager ID?",
-          }
-        ]
-        inquirer.prompt(managerIdQuestion).then((answers) => {
-          connection.connect(function (err) {
-            if (err){
-              console.log(err)
-            }
-            managerArray.splice(2, 0, parseInt(answers.managerId));
-            connection.query("INSERT INTO employee (FIRST_NAME, LAST_NAME, MANAGER_ID, ROLE_ID) VALUES(?, ?, ?, ?);", managerArray,
-              function (error, results) {
-                if (error) {
-                  console.log(`Sorry we we're not able to create the employee due to ${error}`);
-                } else {
-                  console.log(`Success you have been added on to the database, Thanks ${managerArray[0]}!`)
-                }
-              });
-            connection.end();
-          })
-        })
-
-      } else {
-        connection.connect(function (err) {
-          if (err) throw err;
-          connection.query("INSERT INTO employee (FIRST_NAME, LAST_NAME, MANAGER_ID, ROLE_ID) VALUES(?, ?, ?, ?);",
-            [
-              answers.firstName,
-              answers.lastName,
-              answers.manager_id = null,
-              answers.role
-            ],
+      inquirer.prompt(managerIdQuestion).then((answers) => {
+          managerArray.splice(2, 0, parseInt(answers.managerId));
+          connection.query("INSERT INTO employee_tracker.employee (FIRST_NAME, LAST_NAME, MANAGER_ID, ROLE_ID) VALUES(?, ?, ?, ?);", managerArray,
             function (error, results) {
               if (error) {
                 console.log(`Sorry we we're not able to create the employee due to ${error}`);
               } else {
-                console.log(`Success you have added ${answers.firstName} on to the database!`)
-              }
+                console.log(`Success you have been added on to the database, Thanks!`)
+              }    
+              programRestarter();  
             });
-          connection.end();
-        })
-      }
-      // programRestarter();
-    })
-  }
+      })
+    } else {
+      connection.connect(function (err) {
+        if (err) throw err;
+        connection.query("INSERT employee_tracker.employee (FIRST_NAME, LAST_NAME, MANAGER_ID, ROLE_ID) VALUES(?, ?, ?, ?);",
+          [
+            answers.firstName,
+            answers.lastName,
+            answers.manager_id = null,
+            answers.role
+          ],
+          function (error, results) {
+            if (error) {
+              console.log(`Sorry we we're not able to create the employee due to ${error}`);
+            } else {
+              console.log(); // Creating this to give space to programRestarter and message for a cleaner look
+              console.log(`Success you have added ${answers.firstName} on to the database!`)
+             
+            }
+          });
+
+        programRestarter();
+      })
+    }
+
+  })
 }
+
 
 // Adding a Department
-function addDepartments() {
 
-}
+const addDepartments = () =>
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'departmentName',
+      message: "What's the name of the department you would like to add?",
+    },
+  ]).then(answer => {
+    connection.query(
+      'INSERT INTO department (department_name) VALUES (?)',
+      answer.departmentName,
+      function (err, res) {
+        if (err) throw err;
+        console.log(
+          `You have entered ${answer.departmentName} to the database.`
+        );
+        programRestarter()
+      }
+    );
+  });
+
 
 //Adding a Role
-function addRoles() {
+const addRoles = () => {
   inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "What is the name of the role you wish to add?",
-      name: "roleTitle",
-    },
-    {
-      type: "input",
-      message: "What is the salary for this department? (Only the ammount)",
-      name: "roleSalary",
-    },
-  ]) //Here is what I am going to do with the response.
-  .then((response1) => {
-    connection.query("ALTER TABLE = ? FROM DEPARTMENT;", function (error, result) {
-      if (error) {
-        console.log("Where not able to get the departments ", error);
+    .prompt([
+      {
+        type: "input",
+        message: "What role would you like to add?",
+        name: "roleTitle",
+      },
+      {
+        type: "input",
+        message: "Salary for this deparment?",
+        name: "roleSalary",
+      },
+      {
+        type: "input",
+        message: "What is the department ID for this role?",
+        name: "departmentID",
+      },
+    ]).then((response) => {
+      let role = response.roleTitle.toUpperCase();
+      let salary = response.roleSalary;
+      let departmentID = response.departmentID;
+      connection.query('INSERT INTO DEPARTMENT (DEPARTMENT_NAME) VALUES (?)', departmentID, function (error, result) {
+        if (error) {
+          console.log('Failure in the deparment ID you chose', error)
+        } 
       }
-      console.log("Department's");
-      console.table(result);
-    });
-    setTimeout(function () {
-      inquirer
-        .prompt([
-          {
-            type: "input",
-            message: "What is the department ID for this role?",
-            name: "departmentID",
-          },
-        ])
-        .then((response) => {
-          let role = response1.roleTitle;
-          role = role.toUpperCase();
-          let salary = response1.roleSalary;
-          let departmentID = response.departmentID;
-          //This is the query that i am passing to my database, I use '?' to prevent my query from being injected
-          connection.query(
-            "INSERT INTO ROLE (ROLE_TITLE, ROLE_SALARY, DEPARTMENT_ID) VALUES (?, ?, ?);",
-            [role,salary,departmentID],
-            function (error, result) {
-              if (error) {
-                console.log(
-                  "We where not able to create the employee, try again",
-                  error
-                );
-              } else {
-                connection.query(
-                  "SELECT * FROM ROLE WHERE ID=?;",
-                  [result.insertId],
-                  function (error, result) {
-                    if (error) {
-                      console.log(
-                        "There was an error looking for the role after it being created: ",
-                        error
-                      );
-                    }
-                    console.log("The role has been created");
-                    console.table(result);
-                  }
-                );
+      )
+      connection.query(
+        "INSERT INTO ROLE (ROLE_TITLE, ROLE_SALARY, DEPARTMENT_ID) VALUES (?, ?, ?);",
+        [role, salary, departmentID],
+        function (error, result) {
+          if (error) {
+            console.log(
+              "We where not able to create the employee, try again",
+              error
+            );
+          } else {
+            connection.query(
+              "SELECT * FROM ROLE WHERE ID=?;",
+              [result.insertId],
+              function (error, result) {
+                if (error) {
+                  console.log(
+                    "There was an error looking for the role after it being created: ",
+                    error
+                  );
+                }
+                console.log("The role has been created");
+                console.table(result);
+                programRestarter()
               }
+            );
+          }
 
-            }
-          );
-        });
-    }, 200);
-  });
+        }
+      );
+    });
 }
+
 
 // Remove an employee
 function removeEmployee() {
@@ -298,11 +319,84 @@ function removeEmployee() {
   programRestarter();
 }
 // Update employee's role
-function updateRole() {
-  console.log('updated role!')
-  programRestarter();
+const updateEmployee = () => {
 
+  setTimeout(function () {
+    let updateChoices = [
+      "Fist name",
+      "Last name",
+      "Role ID",
+      "Manager ID(1 or null)",
+    ];
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is the ID of the employee you wish to update?",
+          name: "IDUpdate",
+        },
+        {
+          type: "list",
+          message: "What would you like to update?",
+          choices: updateChoices,
+          name: "updateChoice",
+        },
+        {
+          type: "input",
+          message: "Write your change",
+          name: "updateChange",
+        },
+      ])
+      .then((response) => {
+        let updateColumn;
+        switch (response.updateChoice) {
+          case "Fist name":
+            updateColumn = "UPDATE EMPLOYEE SET FIRST_NAME=? WHERE ID=?;";
+            break;
+          case "Last name":
+            updateColumn = "UPDATE EMPLOYEE SET LAST_NAME=? WHERE ID=?;";
+            break;
+          case "Role ID":
+            updateColumn = "UPDATE EMPLOYEE SET ROLE_ID=? WHERE ID=?;";
+            break;
+          case "Manager ID":
+            updateColumn = "UPDATE EMPLOYEE SET MANAGER_ID=? WHERE ID=?;";
+            break;
+          default:
+            break;
+        }
+        connection.query(
+          updateColumn,
+          [response.updateChange, response.IDUpdate],
+          function (error, result) {
+            if (error) {
+              console.log(
+                "There has been a mistake updating the employee: ",
+                error
+              );
+            } else {
+              console.log(
+                "The employee with the ID of",
+                response.IDUpdate,
+                "has been successfully updated!!!!!"
+              );
+              connection.query(
+                "SELECT * FROM EMPLOYEE WHERE ID=?;",
+                [response.IDUpdate],
+                function (error, result) {
+                  console.table(result);
+                }
+              );
+            }
+            setTimeout(function () {
+              programRestarter();
+            }, 200);
+          }
+        );
+      });
+  }, 700);
 }
+
 // Update manager
 function updateManager() {
   console.log('update Manager!');
